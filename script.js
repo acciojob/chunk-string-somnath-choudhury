411bc930cacff29695f4569202ca04b71f304147
Somnath Choudhury <102171721+somnath-choudhury@users.noreply.github.com>
2025-08-13 23:20:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-function stringChop(str, size) {
+function stringChop(str, num) {
   // your code here
+	if (str === null)
+		return null;
+
+	const answerArr = []
+    let n = str.length
+    let newWord = ""
+    for (let i = 0; i < n ; i++) {
+        let ch = str[i];
+
+        newWord += ch
+        if (newWord.length === num) {
+            answerArr.push(newWord)
+            newWord = "";
+        }
+
+    }
+     if (newWord.length > 0) {
+       answerArr.push(newWord);
+     }
+
+	return answerArr;
 }
 
 // Do not change the code below
